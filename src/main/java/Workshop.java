6a5bd304613ab3dd0d865a774b5350674b2e83e9
Andyriu <167091510+MsFabi@users.noreply.github.com>
2025-05-11 22:21:18 -0500

    correccion contarPalabras

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -278,6 +278,9 @@ public class Workshop {
     public int contarPalabras(String cadena) {
         // TODO: Implementar el método para contar el número de palabras en una cadena.
         // Ejemplo: Si cadena = "Este es un test", el resultado debería ser 4.
+        if (cadena == null || cadena.trim().isEmpty()) {
+            return 0;
+        }
         String[] palabras = cadena.split("\\s+");
         return palabras.length;
     }
